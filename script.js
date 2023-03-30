9b59a7622d36560aa99cfc15e3f2dfa8aac3fcf1
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-03-30 16:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function threeSum(arr, target) {
+function threeSum(nums, target) {
 // write your code here
-  
+  nums.sort((a, b) => a - b);
+    let closestSum = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            const currSum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(currSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currSum;
+            }
+            if (currSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return closestSum;
 }
 
 module.exports = threeSum;
